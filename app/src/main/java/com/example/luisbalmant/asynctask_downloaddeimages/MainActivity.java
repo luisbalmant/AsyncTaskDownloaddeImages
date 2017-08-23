863246ec61922cf96bde4075c93e6da44ddfcb10
LuisBalmant <luisbalmant@outlook.com>
2017-08-23 23:18:11 +0100

    App finished

--- app/src/main/java/com/example/luisbalmant/asynctask_downloaddeimages/MainActivity.java
+++ app/src/main/java/com/example/luisbalmant/asynctask_downloaddeimages/MainActivity.java
@@ -1,20 +1,85 @@
 package com.example.luisbalmant.asynctask_downloaddeimages;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myFindViewByIds();
     }
 
     public void downloadImage_Button(View view){
         Toast.makeText(this, "Request sent...", Toast.LENGTH_SHORT).show();
+
+        DownloadTask task = new DownloadTask();
+        Bitmap myImage;
+
+        try {
+            myImage = task.execute("https://www.emotioncard.com.br/wp-content/uploads/2017/07/6217ca093721676961303611047e0670-yorkshire-terrier-puppies-yorkshire-dog.jpg").get();
+
+            imageView.setImageBitmap(myImage);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+    }
+
+    public void myFindViewByIds(){
+        //ImageView
+        imageView = findViewById(R.id.imageView);
 
     }
 
